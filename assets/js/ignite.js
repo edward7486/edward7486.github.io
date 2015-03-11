3d89f7969d2939333c89be9596026f0282376872
edward7486 <enguyen@uw.edu>
2015-03-10 23:09:10 -0700

    Adding Ignite Site to Portfolio

---
+++ assets/js/ignite.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+	// Moving Register Button in Header based on Window Width //
+	if ( $(window).width() <= 1024 ) {
+		
+		$("button.register").remove().prependTo(".container");
+
+	} else if ( $(window).width() >= 1024 ) {
+
+		$("button.register").remove().appendTo("header");
+	
+	}
+
+	//
+
+	// Inserting Close button for side menu
+
+	if ( $(window).width() <= 1024 ) {
+
+		$("ul.nav").prepend("<div id='close'>&#x2715;</div>");
+
+		$("header nav").append("<div id='hamburger'>&#9776;</div>");
+
+	} else {
+
+		$("#close").remove();
+		$("#hamburger").remove();
+
+	}
+
+	//
+
+	// Menu Toggling //
+
+	$("#hamburger").on("click", function(){
+
+		$('header nav ul').addClass( 'open' );
+		$('body').bind( 'touchmove', function(e){ e.preventDefault() });
+
+	});
+
+	$("#close").on("click", function(){
+
+		$('header nav ul').removeClass( 'open' );
+		$('body').unbind( 'touchmove' );
+
+	});
+
+	// Search //
+
+});
