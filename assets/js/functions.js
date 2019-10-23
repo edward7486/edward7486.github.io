d125ac3be25e21a7d4dd269cb3de01b283244d2a
Edward <enguyen@uw.edu>
2019-10-23 04:57:06 -0700

    10-23 update

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,17 @@
 $( document ).ready(function() {
-
   //
-
+  switch (window.location.pathname) {
+	case '/':
+	    $('#work').addClass('active');
+	    break;
+	case '/process/':
+	    $('#process').addClass('active');
+	    break;
+	case '/blog/':
+	    $('#blog').addClass('active');
+	    break;   
+	case '/about/':
+	    $('#about').addClass('active');
+	    break;
+	}
 });
