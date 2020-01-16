fde9ad94de70e47a25a9d3986b083c7a013eb081
Edward <enguyen@uw.edu>
2020-01-16 01:15:12 -0800

    1-16-20 updated style and case studies
    focus on lp templates and design system for now + new visual style

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,17 +1,3 @@
 $( document ).ready(function() {
   //
-  switch (window.location.pathname) {
-	case '/':
-	    $('#work').addClass('active');
-	    break;
-	case '/process/':
-	    $('#process').addClass('active');
-	    break;
-	case '/blog/':
-	    $('#blog').addClass('active');
-	    break;   
-	case '/about/':
-	    $('#about').addClass('active');
-	    break;
-	}
 });
